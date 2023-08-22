03c61085775b305f8fdf1110e4c12388ad98e13e
josephinel6 <127054185+josephinel6@users.noreply.github.com>
2023-08-22 10:05:38 -0700

    Update global.js

--- global.js
+++ global.js
@@ -41,7 +41,7 @@ function openpage(pageName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+     //   tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
