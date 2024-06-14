bfcc093b9d20f74cff05334aa3a9f05c75656e35
Josephine Li <josephinemouse@gmail.com>
2024-06-14 13:39:26 -0700

    initial commit, colors work

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Frame from './components/Frame.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <Frame/>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* :root.light {
+    --color-bg: #eeeff6;
+    --color-menu-text: #4d4d4d;
+    --color-page-bg: white;
+    --color-obj-bg: #f1f2f3;
+    --color-mid-obj-bg: #b0b0b0;
+    --color-mid-obj-hover: #8e8e8e;
+    --color-title-text: #4d4d4d;
+    --color-gen-text: #4d4d4d;
+    --color-menu-hover: darkgrey;
+    --color-title-hover: darkgrey;
+}
+
+:root.dark {
+    --color-bg: #212121;
+    --color-menu-text: #D3D3D3;
+    --color-page-bg: #333333;
+    --color-obj-bg: #403f3f;
+    --color-mid-obj-bg: #535252;
+    --color-mid-obj-hover: rgb(58, 58, 58);
+    --color-title-text: #D3D3D3;
+    --color-gen-text: white;
+    --color-menu-hover: grey;
+    --color-title-hover: grey;
+} */
+</style>
+
+<!-- <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style> -->
---
+++ src/components/Frame.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { RouterLink } from 'vue-router'
+</script>
+
+<script>
+export default{
+    methods: {
+        dayNight() {
+            if (localStorage.getItem("user-theme") === "dark") {
+                localStorage.setItem("user-theme", "light");
+            } else {
+                localStorage.setItem("user-theme", "dark");
+            }
+            document.documentElement.className = localStorage.getItem("user-theme");
+            console.log(localStorage.getItem("user-theme"));
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="menu">
+        <div style="position:absolute; top: 8%; width: 100%; text-align: center;">
+            
+            <RouterLink to="/" class="menutext"> Home </RouterLink>
+            <p class="menutext" onclick="location.href='../projects'"> Projects </p>
+            <p class="menutext" onclick="location.href='../about'"> About </p>
+
+        </div>
+
+        <div style="position:absolute; bottom:3%; width: 100%; text-align: center;">
+            <FontAwesomeIcon :icon="faGithub" href="https://github.com/josephinel6" size="lg"/>
+            <!-- <a href="https://github.com/josephinel6" class="fa fa-github fa-2x" style="text-decoration: none;"></a> -->
+        </div>
+    </div>
+    <div id="topbar">
+        <FontAwesomeIcon :icon="faSun" @click="dayNight()" id="day-night" size="lg"/>
+    </div>
+</template>
+
+<style scoped>
+
+h1,
+h2,
+h3 {
+    color: var(--color-gen-text);
+}
+
+#menu {
+    height: 100%;
+    width: 20%;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    background-color: var(--color-bg);
+}
+
+#topbar {
+    width: 100%;
+    height: 8%;
+    background-color: var(--color-bg);
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    vertical-align:middle;
+    justify-content: right;
+    align-items: center;
+}
+
+.menutext {
+    color: var(--color-menu-text);
+    font-family: sans-serif;
+    font-size: 20px;
+}
+
+.menutext:hover {
+    cursor: pointer;
+    color: var(--color-menu-hover);
+}
+
+.fa-github {
+    color: var(--color-menu-text);
+}
+
+.fa-github:hover {
+    color: var(--color-menu-hover);
+    cursor: pointer;
+}
+#window {
+    background-color: var(--color-page-bg);
+    position: absolute;
+    height: 92%;
+    overflow: hidden;
+    width: 80%;
+    right: 0;
+    bottom: 0;
+}
+
+.tabcontent {
+    height: 100%;
+    width: 78vw;
+    position: relative;
+    overflow-y: scroll;
+    padding: 2.5%;
+    /* margin-top: 2.5%;
+    margin-left: 2.5%; */
+}
+
+
+#day-night {
+    color: var(--color-menu-text);
+    z-index: 2;
+    margin-right: 1vw;
+}
+
+#day-night:hover {
+    cursor: pointer;
+    color: var(--color-menu-hover)
+}
+</style>
