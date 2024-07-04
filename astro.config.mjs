0cfcb03a358c9414252d66b27f5615cada3d2cdd
Josephine Li <josephinemouse@gmail.com>
2024-07-04 15:59:01 -0700

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://josephinel6.github.io",
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
 });
